2cb0af8e3fb96307a0924173bfa3eb46927f8923
Subham Karmakar <subhamk.subs@gmail.com>
2020-11-20 18:57:04 +0530

    Added Pdf Merger Core

---
+++ Components/PdfMerger.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Components
+{
+    class PdfMerger
+    {
+        public static bool IsUploadComplete { get; set; } = false;
+        public static bool IsMergeComplete { get; set; } = false;
+        public static bool IsSubmitComplete { get; set; } = false;
+        public static List<byte[]> Pdfs { get; set; } = new List<byte[]>();
+        public static int TotalFiles { get; set; } = 0;
+        public static string Format { get; } = "data:application/pdf;base64,";
+        public static string FinalMergedPdf { get; set; } = string.Empty;
+    }
+}
---
+++ Components/PdfMergerCore.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using static Components.PdfMerger;
+
+
+namespace Components
+{
+    class PdfMergerCore
+    {
+        public static void MergePdf()
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                PdfDocument pdfDoc = new PdfDocument(new PdfWriter(stream));
+                iText.Kernel.Utils.PdfMerger merger = new iText.Kernel.Utils.PdfMerger(pdfDoc);
+
+                foreach (byte[] pdf in Pdfs)
+                {
+                    PdfDocument currentPdf = new PdfDocument(new PdfReader(new MemoryStream(pdf)));
+                    merger.Merge(currentPdf, 1, currentPdf.GetNumberOfPages());
+                    currentPdf.Close();
+                }
+
+                merger.Close();
+                pdfDoc.Close();
+
+                FinalMergedPdf = Format + Convert.ToBase64String(stream.ToArray());
+            }
+
+            IsSubmitComplete = true;
+            IsMergeComplete = true;
+        }
+
+        public static void ReDo()
+        {
+            IsMergeComplete = false;
+            IsSubmitComplete = false;
+            IsUploadComplete = false;
+            Pdfs.Clear();
+            TotalFiles = 0;
+        }
+
+        public static void Submit() => MergePdf();
+    }
+}
