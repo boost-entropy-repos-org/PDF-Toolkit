3586fad3ba3ae52df08f0d9c7dfa99afb4587763
Subham Karmakar <subhamk.subs@gmail.com>
2020-11-20 19:40:16 +0530

    Added Max File limit

--- Components/PdfMerger.cs
+++ Components/PdfMerger.cs
@@ -9,6 +9,7 @@ namespace Components
         public static bool IsSubmitComplete { get; set; } = false;
         public static List<byte[]> Pdfs { get; set; } = new List<byte[]>();
         public static int TotalFiles { get; set; } = 0;
+        public static int MaxFilesAllowed { get; } = 20;
         public static string Format { get; } = "data:application/pdf;base64,";
         public static string FinalMergedPdfURL { get; set; } = string.Empty;
     }
